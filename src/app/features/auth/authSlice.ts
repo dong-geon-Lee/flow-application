35d19b1bcbb2203654bb04670a7a27fc9582570f
dong-geon-Lee <ch9716@naver.com>
2023-08-03 16:55:06 +0900

    user 리덕스툴킷 상태 구축

---
+++ src/app/features/auth/authSlice.ts
@@ -0,0 +1,41 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { getAuthUserList } from "api";
+import * as T from "@types";
+
+const initialState: any = {
+  userLists: [],
+  userInfo: [],
+  message: "",
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout(state, action) {
+      state.username = "";
+      state.email = "";
+      state.password = "";
+      state.authStatus = false;
+      state.message = action.payload;
+    },
+    authUserLogin(state, action) {
+      state.userInfo = action.payload;
+    },
+    refreshUserLists(state, action) {
+      state.userLists = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(
+      getAuthUserList.fulfilled,
+      (state: any, action: PayloadAction) => {
+        state.userLists = action.payload;
+      }
+    );
+  },
+});
+
+export const { logout, authUserLogin, refreshUserLists } = authSlice.actions;
+
+export default authSlice.reducer;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import customerReducer from "./features/customer/customerSlice";
+import authReducer from "./features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     customer: customerReducer,
+    auth: authReducer,
   },
 });
 
