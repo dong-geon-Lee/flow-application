0ed5404285f6d0c52380428e83abbaed8a3a8336
dong-geon-Lee <ch9716@naver.com>
2023-08-08 17:56:25 +0900

    PrivateRoutes 네비게이트 조건 수정

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -3,14 +3,14 @@ import { useSelector } from "react-redux";
 import { Outlet, Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoutes = () => {
-  const auth = useSelector((state: RootState) => state.auth.userInfo);
-  const isLogined = localStorage.getItem("authToken");
+  const auth = useSelector((state: RootState) => state.auth.authUser);
+  const isLogined: any = localStorage.getItem("token");
 
   const location = useLocation();
   const { pathname, search } = location;
   const from = pathname + search;
 
-  return auth.authStatus || isLogined ? (
+  return auth?.userName || isLogined ? (
     <Outlet />
   ) : (
     <Navigate to="/login" state={{ from }} />
