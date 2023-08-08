99eaba89610570770bcbf2e01b59f750eb7bb278
dong-geon-Lee <ch9716@naver.com>
2023-08-08 17:58:49 +0900

    리덕스툴킷 Login API 적용

--- src/app/features/auth/authSlice.ts
+++ src/app/features/auth/authSlice.ts
@@ -1,46 +1,24 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { getAuthUserList } from "api";
-import * as T from "@types";
+import { createSlice } from "@reduxjs/toolkit";
 
-const orderedUsers = JSON.parse(localStorage.getItem("users") || "[]");
-const orderedUserInfo = JSON.parse(localStorage.getItem("singleUser") || "[]");
+const currentUser = JSON.parse(localStorage.getItem("authUser") || "{}");
 
 const initialState: any = {
-  userLists: orderedUsers ? orderedUsers : [],
-  userInfo: orderedUserInfo ? orderedUserInfo : [],
-  message: "",
-  token: localStorage.getItem("authToken")
-    ? localStorage.getItem("authToken")
-    : null,
+  authUser: currentUser ? currentUser : null,
 };
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    logout(state, action) {
-      state.userInfo = [];
-      state.message = action.payload;
+    loginUser(state, action) {
+      state.authUser = action.payload;
     },
-    authUserLogin(state, action) {
-      state.userInfo = action.payload;
-      state.token = localStorage.getItem("authToken");
+    logout(state) {
+      state.authUser = null;
     },
-    refreshUserLists(state, action) {
-      state.userLists = action.payload;
-      state.token = localStorage.getItem("authToken");
-    },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(
-      getAuthUserList.fulfilled,
-      (state: any, action: PayloadAction) => {
-        state.userLists = action.payload;
-      }
-    );
   },
 });
 
-export const { logout, authUserLogin, refreshUserLists } = authSlice.actions;
+export const { loginUser, logout } = authSlice.actions;
 
 export default authSlice.reducer;
